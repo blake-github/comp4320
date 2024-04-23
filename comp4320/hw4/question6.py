c678a75746587f527c7a7d502b76b6d84fdc6d39
Blake <89502374+blake-github@users.noreply.github.com>
2024-04-23 16:05:16 -0500

    Uploaded completed assignments

---
+++ comp4320/hw4/question6.py
@@ -0,0 +1,27 @@
+
+
+# Question 6 
+# Exhaustive key search for shift cipher
+# Blake Moore
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+inputString = "BEEAKFYDJXUQYHYJIQRYHTYJIQFBQDUYJIIKFUHCQD"
+x = 0
+while x < 26:
+    x = x + 1
+    inputString = inputString.upper()
+    decrypted = ""
+    for character in inputString:
+        found = alphabet.find(character)
+        newLocation = found - x
+        if character in alphabet:
+            decrypted = decrypted + alphabet[newLocation]
+        else:
+            decrypted = decrypted + character
+    cipherShift = str(x)
+    print("Cipher shift: " + cipherShift)
+    print("Decrypted message:")
+    print(decrypted + "\n")
+    
+    
+    
